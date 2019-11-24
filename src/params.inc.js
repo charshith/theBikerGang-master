84baf01e9188bd1c0c9a099029cb9e5a1a42d86a
charshith <54497417+charshith@users.noreply.github.com>
2019-11-24 15:24:50 +0530

    Add files via upload

---
+++ src/params.inc.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+        apiKey: "AIzaSyB_qWbVqoRJfr7MlVNfp7kCDKhyCoufDV0",
+        authDomain: "thebikergang-ca126.firebaseapp.com",
+        databaseURL: "https://thebikergang-ca126.firebaseio.com",
+        projectId: "thebikergang-ca126",
+        storageBucket: "thebikergang-ca126.appspot.com",
+        messagingSenderId: "199239557387",
+        appId: "1:199239557387:web:2b16200138f4f038adc20e",
+        measurementId: "G-10HNLF4PVG"
+    };
